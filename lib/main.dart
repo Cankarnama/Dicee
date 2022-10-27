e93143b9af4f0e1cf8efd2c9eb995986a77ef55b
Your Name <you@example.com>
2022-10-27 06:28:40 +0000

    On pressed, Set state,math. dart library also final commit/

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-
-        backgroundColor:Colors.red,
-
-
+        backgroundColor: Colors.red,
         appBar: AppBar(
           //elevation: 20,
           backgroundColor: Colors.red,
@@ -16,35 +14,60 @@ void main() {
             ),
           ),
         ),
-
         body: Dicepage()),
   ));
 }
 
-class Dicepage extends StatelessWidget {
+class Dicepage extends StatefulWidget {
+  @override
+  _DicepageState createState() => _DicepageState();
+}
+
+class _DicepageState extends State<Dicepage> {
+  int l = 1;
+  int r = 1;
+  void changedicenumber() {
+    setState(() {
+      r = Random().nextInt(6) + 1;
+      l = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              onPressed: () {  },
+              onPressed: () {
+                setState(
+                  () {
+                    changedicenumber;
+                  },
+                );
+              },
               child: Image(
-                image: AssetImage('images/dice1.png'),
-              ),
-            ),
-         ),
-          Expanded(
-            child: FlatButton(
-              onPressed: (){},
-              child: Image(
-                image: AssetImage('images/dice1.png'),
+                image: AssetImage('images/dice$l.png'),
               ),
             ),
           ),
-
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                // the set state is used to mark the variable that is placed in it as
+                //dirty ,everywhere it goes and is seen
+                setState(
+                  () {
+                    changedicenumber;
+                  },
+                );
+              },
+              child: Image(
+                image: AssetImage('images/dice$l.png'),
+              ),
+            ),
+          ),
         ],
       ),
     );
